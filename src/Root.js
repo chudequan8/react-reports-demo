260c114dcb744ed01f35ffe8c8c2891060bd5377
褚德权 <chudequan_48@163.com>
2019-10-12 10:45:11 +0800

    ##德权## 路由模式改为哈希模式

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from '@material-ui/styles';
 import reducer from "./reducers";
 import theme from './utils/theme';
